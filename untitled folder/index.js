0c8043995a51394f8256a1756479f3ceb2dff9fa
kumarajay0412 <66991625+kumarajay0412@users.noreply.github.com>
2021-05-21 21:21:43 +0530

    Fully done jwt and backend

---
+++ untitled folder/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/postRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+app.get('/', (req, res)=>{
+    res.send('Hello to Backend');
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(PORT, () => {
+            console.log(`Server started on port: ${PORT}`);
+        }))
+        .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);        
