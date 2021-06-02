a62607d491122c74028ee51500140b3646547c5b
kumarajay0412 <66991625+kumarajay0412@users.noreply.github.com>
2021-06-02 18:01:39 +0530

    Update

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Instagram</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
